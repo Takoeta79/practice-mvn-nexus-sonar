648d86e7ff7282777d7c4eeb88450ecc55ca9b5e
takoe <takoetadaniel@yahoo.com>
2023-12-26 00:50:27 +0100

    correction

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-String name = "vamsi";
-system.out.println(name);
 }
